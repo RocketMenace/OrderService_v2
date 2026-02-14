3c10fe164c65be2ff4536ad4291355bf5bc39d0a
RocketMenace <grizzlygit777@gmail.com>
2026-02-14 21:37:43 +0300

    feat(domain)
    Implement sku validation

--- src/app/domain/services/batch.py
+++ src/app/domain/services/batch.py
@@ -6,6 +6,9 @@ from app.domain.value_objects import BatchID, OrderLine
 
 
 class BatchService:
+    async def is_valid_sku(self, *, sku: str, batches: list[Batch]) -> bool:
+        return sku in {batch.sku for batch in batches}
+
     async def allocate(self, *, line: OrderLine, batches: list[Batch]) -> BatchID:
         try:
             batch = await anext(self._find_batches(line=line, batches=batches))
