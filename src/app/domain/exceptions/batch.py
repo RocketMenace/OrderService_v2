089b5a8f50a15828ce219339cbed3e3ba7f76ab1
RocketMenace <grizzlygit777@gmail.com>
2026-02-14 21:21:11 +0300

    feat(domain)
    Implement invalid sku exception

--- src/app/domain/exceptions/batch.py
+++ src/app/domain/exceptions/batch.py
@@ -5,3 +5,9 @@ class OutOfStockError(DomainError):
     def __init__(self, sku: str):
         message = f"Item with {sku} out of stock"
         super().__init__(message)
+
+
+class InvalidSKUError(DomainError):
+    def __init__(self, sku: str):
+        message = f"Item with {sku} is not found"
+        super().__init__(message)
