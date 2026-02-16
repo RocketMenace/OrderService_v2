ca616097824dafbc9421cd8f3a33ad43ae3386f5
RocketMenace <grizzlygit777@gmail.com>
2026-02-16 13:07:28 +0300

    feat(application)
    Implement UoW interface

--- src/app/application/interfaces/uow.py
+++ src/app/application/interfaces/uow.py
@@ -1,7 +1,15 @@
 from typing import Protocol, Self
 
+from app.application.interfaces.repository import (
+    BatchRepositoryProtocol,
+    OrderRepositoryProtocol,
+)
+
 
 class UnitOfWorkProtocol(Protocol):
+    batches: BatchRepositoryProtocol
+    orders: OrderRepositoryProtocol
+
     async def __aenter__(self) -> Self: ...
     async def __aexit__(self, exc_type, exc_val, exc_tb): ...
     async def commit(self) -> None: ...
